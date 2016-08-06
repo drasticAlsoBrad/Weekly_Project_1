ecebdcaecd1a3f88272ba7336f59ccccd0139536
Brad Bottoms <drastic311@gmail.com>
2016-08-05 23:42:00 -0400

    added timing
    added timing

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function getTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(getTime, 500);
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+}
+
+/*function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}*/
